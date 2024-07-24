18866b704a7bd2c102f48a9fc7e2e32b484c9de8
aniketch07 <caniket1307@gmail.com>
2024-07-24 20:04:09 +0530

    deploy zkevm testnet

--- contracts/contracts/Contract.sol
+++ contracts/contracts/Contract.sol
@@ -6,20 +6,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GenesisGalleria is ERC721URIStorage, Ownable() {
+contract GenesisGalleria is ERC721URIStorage {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    address _owner;
+    
 
-    uint256 public mintingFee = 0.0001 ether;
+   
 
-    constructor() ERC721("GenesisGalleria", "GG"){
-        _owner=msg.sender;
-    }
+    constructor() ERC721("GenesisGalleria", "GG"){}
 
     function mint(string memory tokenURI) payable public returns (uint256) {
-        require(msg.value >= mintingFee, "GenesisGalleria: Insufficient minting fee");
+        
 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
@@ -29,11 +27,5 @@ contract GenesisGalleria is ERC721URIStorage, Ownable() {
         return newItemId;
     }
 
-    function withdrawFunds() public onlyOwner {
-      
-        uint256 balance = address(this).balance;
-        require(balance > 0, "GenesisGalleria: Balance is zero");
-        
-        payable(msg.sender).transfer(balance);
-    }
+    
 }
